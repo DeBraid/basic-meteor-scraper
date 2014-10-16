7498750d60e45e19da492b3745094a1553182cc0
DeBraid <de.braid@gmail.com>
2014-10-16 11:28:20 -0400

    changed title

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ Meteor.methods({
         result = Meteor.http.get("http://www.timeanddate.com/worldclock/city.html?n=136");
         $ = cheerio.load(result.content);
         CurrentTime = $('#ct').html();
-        console.log("CurrentTime", CurrentTime);
         return CurrentTime;
     }
 });
