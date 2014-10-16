afb527d03b47bd07fc945391ee29e2846e90dd9d
DeBraid <de.braid@gmail.com>
2014-10-16 18:02:03 -0400

    printing out list of security names

--- client/main.js
+++ client/main.js
@@ -6,6 +6,20 @@ Meteor.call("getTime", function(error, result) {
   Session.set("currentTime", time);
 });
 
+Meteor.call("etfTitles", function(error, result) {
+  if (error)
+      console.log(error)
+  var title = result;
+
+  Session.set("etfTitles", result);
+});
+
+Template.etfs.helpers({
+  etfTitles: function () {
+    return Session.get("etfTitles");
+  }
+}); 
+
 Template.whatTime.currTime = function (arg) {
   return Session.get("currentTime");
 };
--- server/server.js
+++ server/server.js
@@ -6,5 +6,22 @@ Meteor.methods({
         $ = cheerio.load(result.content);
         CurrentTime = $('#ct').html();
         return CurrentTime;
+    },
+    etfTitles: function () {
+        result = Meteor.http.get("http://en.wikipedia.org/wiki/List_of_Canadian_exchange-traded_funds");
+        $ = cheerio.load(result.content);
+        var arr = [];
+        $('#mw-content-text > ul:nth-child(n) > li:nth-child(n)').map(function () {
+          arr.push($(this).text())
+        })
+        console.log(arr)
+        return arr; 
+        // var winning = arr.map(function ( i ) {
+        //   var singles = [];
+        //   singles.push(i.split(','));
+        //   return singles;
+        // });
+        // return winning;
     }
 });
+
