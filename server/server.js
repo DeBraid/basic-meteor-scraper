1971aee86a9399c1219838a74b1490e9dc44f473
DeBraid <de.braid@gmail.com>
2015-02-20 13:21:59 -0500

    removed rubbish from server.js

--- server/server.js
+++ server/server.js
@@ -1,12 +1,6 @@
-  var cheerio =  Meteor.require('cheerio');
+var cheerio =  Meteor.require('cheerio');
 
 Meteor.methods({
-    getTime: function () {
-        result = Meteor.http.get("http://www.timeanddate.com/worldclock/city.html?n=136");
-        $ = cheerio.load(result.content);
-        CurrentTime = $('#ct').html();
-        return CurrentTime;
-    },
     etfList: function () {
       var arr = [];
       var badString = "List";
@@ -25,17 +19,10 @@ Meteor.methods({
       return sorted; 
   },
   quandl: function () {
-    // var url = "http://finance.yahoo.com/q/pm?s=" + ticker + ".TO"
     var url = "https://www.quandl.com/api/v1/datasets/YAHOO/TSX_ZEB_TO.json?auth_token=";
     var authKey = Meteor.settings.quandlKey;
-    console.log("authKey", authKey);
     var response = Meteor.http.get( url + authKey );
     return response;
-      
-      // $ = cheerio.load(result.content);
-      // var data = $('#yfncsumtab > tbody > tr:nth-child(2) > td:nth-child(1) > table.yfnc_datamodoutline1 > tbody > tr > td > table > tbody > tr:nth-child(1) > td.yfnc_datamoddata1').html();
-      // return data;
-    
   }  
 });
 
